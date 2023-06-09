f0cc7a3578d69cf5c8da789cd4ce8e8805590c1d
Александр Угловский <alex3angle@gmail.com>
2023-06-09 20:43:10 +0400

    comment all unsupported network properties

--- dws/provider/client/network.go
+++ dws/provider/client/network.go
@@ -1,19 +1,22 @@
 package client
 
-import "context"
+import (
+	"context"
+	"errors"
+)
 
 func (c *DWSClient) CreateNetwork(ctx context.Context, network *NetworkConfig) (*NetworkConfig, error) {
-
+	return nil, errors.New("not implemented")
 }
 
 func (c *DWSClient) GetNetwork(ctx context.Context, id string) (*NetworkConfig, error) {
-
+	return nil, errors.New("not implemented")
 }
 
 func (c *DWSClient) UpdateNetwork(ctx context.Context, id string, network *NetworkConfig) (*NetworkConfig, error) {
-
+	return nil, errors.New("not implemented")
 }
 
 func (c *DWSClient) DeleteNetwork(ctx context.Context, id string) error {
-
+	return errors.New("not implemented")
 }
--- dws/provider/client/network_types.go
+++ dws/provider/client/network_types.go
@@ -2,27 +2,25 @@ package client
 
 import (
 	"net"
-
-	wireGuard "golang.zx2c4.com/wireguard/wgctrl/wgtypes"
 )
 
 type NetworkConfig struct {
-	ID           string
-	Name         string
-	Description  string
-	Nodes        []uint32
-	IPRange      net.IPNet
-	AddWGAccess  bool
-	SolutionType string
-	NodesIPRange map[uint32]net.IPNet
+	ID          string
+	Name        string
+	Description string
+	IPRange     net.IPNet
+	// Nodes        []uint32
+	// AddWGAccess  bool
+	// SolutionType string
+	// NodesIPRange map[uint32]net.IPNet
 
 	// computed
-	AccessWGConfig   string
-	ExternalIP       *net.IPNet
-	ExternalSK       wireGuard.Key
-	PublicNodeID     uint32
-	NodeDeploymentID map[uint32]uint64
+	// AccessWGConfig   string
+	// ExternalIP       *net.IPNet
+	// ExternalSK       wireGuard.Key
+	// PublicNodeID     uint32
+	// NodeDeploymentID map[uint32]uint64
 
-	WGPort map[uint32]int
-	Keys   map[uint32]wireGuard.Key
+	// WGPort map[uint32]int
+	// Keys   map[uint32]wireGuard.Key
 }
